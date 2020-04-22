1cef51912e06f3ef2297c618c0eb82ee49e10375
Alessio D'Innocenti <alessio.dinnocenti@gmail.com>
2020-04-22 23:42:28 +0200

    rimossi file

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,16 @@
 import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
-import Root from './containers/Root';
-import { configureStore, history } from './store/configureStore';
 import './app.global.css';
+import Root from './containers/Root';
 import { setupPrinterRenderer } from './utils/printer';
-
-const store = configureStore();
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
 
 document.addEventListener('DOMContentLoaded', () =>
   render(
     <AppContainer>
-      <Root store={store} history={history} />
+      <Root />
     </AppContainer>,
     document.getElementById('root')
   )
